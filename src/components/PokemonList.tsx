6a0e93bb3ab9c65243f7989357e460ea7253e919
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-12-20 18:21:59 -0300

    Added some styles and the spinner

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './PokemonList.css';
 import { IonCard, IonCardContent, IonCardHeader, IonGrid, IonRow, IonCol, } from '@ionic/react';
 
@@ -22,72 +22,91 @@ interface ContainerProps {}
 
 const PokemonList: React.FC<ContainerProps> = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [isCharging, setIsCharging] = useState<Boolean>(false);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://beta.pokeapi.co/graphql/v1beta', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            query: `query MyQuery {
-              pokemon_v2_pokemon {
-                id
-                name
-                height
-                weight
-                pokemon_v2_pokemonabilities {
-                  pokemon_v2_ability {
-                    name
-                  }
-                }
-                base_experience
-                pokemon_v2_pokemonsprites {
-                  sprites
+  const fetchData = async () => {
+    setIsCharging(true)
+    try {
+      const response = await fetch('https://beta.pokeapi.co/graphql/v1beta', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `query MyQuery {
+            pokemon_v2_pokemon {
+              id
+              name
+              height
+              weight
+              pokemon_v2_pokemonabilities {
+                pokemon_v2_ability {
+                  name
                 }
               }
+              base_experience
+              pokemon_v2_pokemonsprites {
+                sprites
+              }
             }
-            `,
-          }),
-        });
+          }
+          `,
+        }),
+      });
 
-        if (!response.ok) {
-          throw new Error('There was an error');
-        }
-
-        const data = await response.json();
-
-        if (data.errors) {
-          throw new Error(data.errors[0].message);
-        }
-
-        const shuffledPokemons = [...data.data.pokemon_v2_pokemon];
-        for(let i=shuffledPokemons.length - 1; i>0; i-- ){
-          const j=Math.floor(Math.random() * (i + 1))
-          const aux = shuffledPokemons[i]
-          shuffledPokemons[i] = shuffledPokemons[j]
-          shuffledPokemons[j] = aux
-        }
-        setPokemons(shuffledPokemons);
-
-      } catch (error) {
-        console.error('Error fetching Pokemon data:', error);
+      if (!response.ok) {
+        throw new Error('There was an error');
       }
-    };
 
+      const data = await response.json();
+
+      if (data.errors) {
+        throw new Error(data.errors[0].message);
+      }
+
+      const shuffledPokemons = [...data.data.pokemon_v2_pokemon];
+      for(let i=shuffledPokemons.length - 1; i>0; i-- ){
+        const j=Math.floor(Math.random() * (i + 1))
+        const aux = shuffledPokemons[i]
+        shuffledPokemons[i] = shuffledPokemons[j]
+        shuffledPokemons[j] = aux
+      }
+      setPokemons(shuffledPokemons);
+    } catch (error) {
+      console.error('Error fetching Pokemon data:', error);
+    }
+
+    setIsCharging(false);
+  };
+
+  useEffect(() => {
     fetchData();
   }, []);
+
+  const handleScroll = () => {
+    const container = containerRef.current;
+    if (container) {
+      const { scrollTop, clientHeight, scrollHeight } = container;
+      if (scrollTop + clientHeight >= scrollHeight - 20) {
+        setIsCharging(true);
+      }
+    }
+  };
 
 
   return (
     <div id="container">
-      <IonGrid>
+      {isCharging ? (
+        <div id='modal'>
+          <img src='/pokeball.png' alt="loading" />
+        </div>
+      ) : (
+        <IonGrid>
         <IonRow>
-          {pokemons.map((pokemon) => (
-            <IonCol size='4'>
-                <IonCard key={pokemon.id}>
+          {pokemons && pokemons.map((pokemon) => (
+            <IonCol key={pokemon.id} size='4'>
+                <IonCard id='pokemonCard'>
                   <IonCardHeader>
                     <h2>{pokemon.name}</h2>
                   </IonCardHeader>
@@ -104,8 +123,10 @@ const PokemonList: React.FC<ContainerProps> = () => {
           ))}
         </IonRow>
       </IonGrid>
+      )}
     </div>
   );
+  ;
 };
 
 export default PokemonList;
